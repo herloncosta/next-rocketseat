d0f4a70baa7a0c8b09d1093780f426ade1c3b679
herloncosta <herlon36@gmail.com>
2023-12-09 08:42:21 -0300

    updating loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,9 @@
 // Essa página é exibida quando ocorrer carregamento de algum recurso
 
 export default function Loading() {
-    return <h1>Carregando...</h1>
+    return (
+        <main className="flex items-center justify-center">
+            <h1 className="text-2xl">Carregando...</h1>
+        </main>
+    )
 }
